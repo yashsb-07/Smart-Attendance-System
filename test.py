a5372e8d398c7bbc723cdbd3aad4812c160ce31c
yashsb-07 <yashsbansode07@gmail.com>
2025-10-05 15:02:18 +0530

    updates

--- test.py
+++ test.py
@@ -1,22 +1,33 @@
-import cv2
+# import cv2
 
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)  # Use DirectShow
-if not cap.isOpened():
-    print("❌ Error: Could not open webcam.")
-    exit()
+# cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)  # Use DirectShow
+# if not cap.isOpened():
+#     print("❌ Error: Could not open webcam.")
+#     exit()
 
-print("📸 Opening webcam... Press 'q' to exit.")
+# print("📸 Opening webcam... Press 'q' to exit.")
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        print("❌ Error: Failed to capture frame.")
-        break
+# while True:
+#     ret, frame = cap.read()
+#     if not ret:
+#         print("❌ Error: Failed to capture frame.")
+#         break
 
-    cv2.imshow("Camera Test", frame)
+#     cv2.imshow("Camera Test", frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
 
-cap.release()
-cv2.destroyAllWindows()
+# cap.release()
+# cv2.destroyAllWindows()
+
+
+# import bcrypt
+
+# hashed_password = b"$2b$12$LHwwwpUCsdTMxF8BAR/C9.te2.g0CGkgEW1S7njTtwImoPdupwYxq"
+# plain_password = b"yashsb07"  # Try your guess
+
+# if bcrypt.checkpw(plain_password, hashed_password):
+#     print("✅ Password matches!")
+# else:
+#     print("❌ Password does not match.")
