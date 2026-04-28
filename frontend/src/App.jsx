f9cb8e2b22b1d530de1d1761329f7b98c03f3335
yashsb-07 <yashsbansode07@gmail.com>
2026-04-28 22:14:53 +0530

    chore: initial project setup with django and react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Tailwind is Working 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App;
