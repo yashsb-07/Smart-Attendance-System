102c364ca1c50b65fd6b609b277b6d97b7d7fca8
yashsb-07 <yashsbansode07@gmail.com>
2026-05-07 21:38:31 +0530

    feat: added login API integration with JWT handling

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class LoginView(TokenObtainPairView):
+    pass
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,30 @@
+import { useState } from "react";
+import API from "../services/api";
+import { useNavigate } from "react-router-dom";
+
+
 function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        try {
+        const res = await API.post("login/", {
+            email,
+            password,
+        });
+
+        localStorage.setItem("token", res.data.access);
+        navigate("/dashboard");
+
+        } catch (error) {
+        alert("Invalid credentials");
+        }
+    };
+
   return (
     <div className="flex items-center justify-center min-h-[80vh]">
 
@@ -14,7 +40,7 @@ function Login() {
         </p>
 
         {/* Form */}
-        <form className="mt-6 space-y-4">
+        <form onSubmit={handleLogin} className="mt-6 space-y-4">
 
           {/* Email */}
           <div>
@@ -22,6 +48,8 @@ function Login() {
             <input
               type="email"
               placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               className="w-full mt-1 px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
@@ -32,6 +60,8 @@ function Login() {
             <input
               type="password"
               placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               className="w-full mt-1 px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
