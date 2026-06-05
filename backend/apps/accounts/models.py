69279b6cbca9178546ef97292eb36d63c4799b73
yashsb-07 <yashsbansode07@gmail.com>
2026-06-05 20:23:27 +0530

    feat(auth): create accounts app and custom user model

--- backend/apps/accounts/models.py
+++ backend/apps/accounts/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('teacher', 'Teacher'),
+        ('student', 'Student'),
+    )
+
+    email = models.EmailField(
+        unique=True
+    )
+
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default='student'
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    def __str__(self):
+        return self.username
--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     "corsheaders",
+
+    #Local Apps
+    'apps.accounts',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = 'accounts.User'
