6a22f308c5d50c5518d7012fd9b2948b1f6344d8
yashsb-07 <yashsbansode07@gmail.com>
2026-06-28 22:49:52 +0530

    Updated departmentservice.js file

--- frontend/src/services/departmentService.js
+++ frontend/src/services/departmentService.js
@@ -1,19 +1,19 @@
 import api from "../api/axios";
 import { API_ENDPOINTS } from "../api/endpoints";
 
-export const getDepartments = async () => {
-    const response = await api.get(
-        API_ENDPOINTS.DEPARTMENTS.LIST
-    );
+export const getDepartments = async () => { 
+    const response = await api.get( 
+        API_ENDPOINTS.DEPARTMENTS.LIST 
+    ); 
+    
+    return response.data; 
+}; 
 
-    return response.data;
-};
-
-export const createDepartment = async (data) => {
-    const response = await api.post(
-        API_ENDPOINTS.DEPARTMENTS.LIST,
-        data
-    );
-
-    return response.data;
+export const createDepartment = async (data) => { 
+    const response = await api.post( 
+        API_ENDPOINTS.DEPARTMENTS.LIST, 
+        data 
+    ); 
+    
+    return response.data; 
 };
