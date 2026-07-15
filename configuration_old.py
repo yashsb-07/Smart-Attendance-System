42f28fb02574831ac1c44b8e3c5241c8e1e36323
Yash Bansode <yashsbansode07@gmail.com>
2026-07-15 22:48:11 +0530

    Comment out DB configuration in configuration_old.py
    Comment out database configuration variables.

--- configuration_old.py
+++ configuration_old.py
@@ -1,4 +1,4 @@
-DB_HOST = "localhost"
-DB_USER = "root"
-DB_PASSWORD = "R!t!k@*1606."
-DB_NAME = "smart_attendance"
+# DB_HOST = "localhost"
+# DB_USER = "root"
+# DB_PASSWORD = "R!t!k@*1606."
+# DB_NAME = "smart_attendance"
