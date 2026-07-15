29de7b960227a8baf3f0dfd0dd22a54a8fbae41d
Yash Bansode <yashsbansode07@gmail.com>
2026-07-15 22:49:45 +0530

    Comment out Config class in config.py
    Comment out the Config class and its attributes

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-class Config:
-    SECRET_KEY = "attendance_secret_key"
+# class Config:
+#     SECRET_KEY = "attendance_secret_key"
 
-    MYSQL_HOST = "localhost"
-    MYSQL_USER = "root"
-    MYSQL_PASSWORD = "R!t!k@*1606."
-    MYSQL_DATABASE = "attendance_system"
+#     MYSQL_HOST = "localhost"
+#     MYSQL_USER = "root"
+#     MYSQL_PASSWORD = "R!t!k@*1606."
+#     MYSQL_DATABASE = "attendance_system"
