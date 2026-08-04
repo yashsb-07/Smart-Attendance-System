174e5276d2c2e04907982fd96ce6309acf5e5006
yashsb-07 <yashsbansode07@gmail.com>
2026-08-05 00:02:54 +0530

    chore(accounts): register accounts application in Django settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -31,12 +31,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django Applications
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-Party Applications
+    "rest_framework",
+
+    # Local Applications
+    "apps.accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
