367083925c6fc31814bb2f78e6853bb27871a2f6
yashsb-07 <yashsbansode07@gmail.com>
2026-08-05 12:42:19 +0530

    chore(api): configure Django REST Framework defaults

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -129,3 +129,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
+}
