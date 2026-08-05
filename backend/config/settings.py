2f52b5d856c819ae76fb2277e0b7a2459311e206
yashsb-07 <yashsbansode07@gmail.com>
2026-08-05 12:25:41 +0530

    chore(database): configure PostgreSQL connection

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -81,9 +81,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
+        "PORT": config("DB_PORT"),
     }
 }
 
